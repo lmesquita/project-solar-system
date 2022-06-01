9388669cd3ee17e2799b51af714b78517a56bfb6
Lucas Mesquita <lucas.cas.mesquita@gmail.com>
2022-06-01 19:01:08 -0300

    Subindo projeto

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+        {
+          planets.map((planeta) => (<PlanetCard
+            key={ planeta.name }
+            planetName={ planeta.name }
+            planetImage={ planeta.image }
+          />))
+        }
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
